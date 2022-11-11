7c753ac96c73a90b1db7d1c4737026e95883a600
Patryk-Jastrzebski <patryk.jastrzebski@iteo.com>
2022-11-11 10:23:05 +0100

    Initial Commit

---
+++ cchat/cchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  cchatApp.swift
+//  cchat
+//
+//  Created by Patryk Jastrzębski on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct cchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
